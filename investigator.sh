921df4db04039fe3266bce225daa06407dead79d
Oliver Djursing <Oliverdjursing@gmail.com>
2024-05-03 19:12:20 +0200

    refactor: switch to optional params

--- investigator.sh
+++ investigator.sh
@@ -2,51 +2,60 @@
 set -euo pipefail
 
 # Default values for parameters
-grep_string="${1:-fix\|bug}"
-since_date="${2:-1 year}"
+grep_string='fix|bug'
+since_date='1 year'
 
+# Enhanced Help function with more details
 Help() {
-    echo "Usage: $0 [GREP_STRING] [SINCE_DATE]"
-    echo "Searches the git log for commits matching certain criteria and lists the occurence count of each file in the commits."
+    echo "Usage: $0 [-g|--grep GREP_STRING] [-s|--since SINCE_DATE]"
+    echo "Searches the git log for commits matching certain criteria and lists the occurrence count of each file that has been involved in descending order."
     echo
-    echo "Parameters:"
-    echo "  GREP_STRING   Regex pattern to grep in commit messages. Follows git log --grep definition. Default is 'fix\|bug'."
-    echo "  SINCE_DATE    Time frame for the log search. Follows git log --since definition. Default is '1 year'."
-    echo 
+    echo "Options:"
+    echo "  -g, --grep   Extended Regex pattern to grep in commit messages. Follows git log -E --grep definition. Default is 'fix|bug'."
+    echo "  -s, --since  Time frame for the log search. Follows git log --since definition. Default is '1 year'."
+    echo
     echo "Examples:"
-    echo "  $0 'feature\|add' '1 month ago'  # Return all files and number of occurences where commit message contained 'feature' or 'add' in the past month."
+    echo "  $0 --grep 'feature|add' --since '1 month ago'  # Search all commits where commit message contains 'feature' or 'add' in the past month."
     echo
 }
+
 
 # Helper function to check if a command exists on the system
 command_exists() {
     command -v "$1" >/dev/null 2>&1
 }
 
-# Check if necessary commands exists on the system
+# Check necessary commands exists on system before proceeding
 if ! command_exists git || ! command_exists sed; then
     echo "Error: Required commands are not installed. Please make sure 'git' and 'sed' are available." >&2
     exit 1
 fi
 
 # Parse options
-while getopts ":h" option; do
-    case $option in
-        h) # display Help
+while [[ "$#" -gt 0 ]]; do
+    case "$1" in
+        -g|--grep)
+            grep_string="$2"
+            shift 2
+            ;;
+        -s|--since)
+            since_date="$2"
+            shift 2
+            ;;
+        -h|--help)
             Help
             exit 0
             ;;
-        \?) # Invalid option
-            echo "Invalid option: -$OPTARG" >&2
+        *)
+            echo "Unknown option: $1" >&2
             Help
             exit 1
             ;;
     esac
 done
 
-
 # Main command
-git log --grep "${grep_string}" --name-only --pretty=format: --since "${since_date}" |
+git log -E --grep "${grep_string}" --name-only --pretty=format: --since "${since_date}" |
     sed -r "/^\n*$/d" |
     sort |
     uniq -c |
